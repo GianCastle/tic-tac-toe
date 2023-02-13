bf0f0109aa9848a6196647505aeb97959675330c
Giancarlos Castillo <gc.c@outlook.com>
2023-02-13 08:06:51 -0400

    feat: adds simple navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import Board from './components/Board'
 function App() {
   return (
     <div className="App">
+
+      <ul className='nav'>
+        <li className='nav-item'>
+          <a href='https://giancastle.com' className='nav-link'>
+            My Blog
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='https://www.linkedin.com/in/gco-c/' className='nav-link'>
+            Linkedin
+          </a>
+        </li>
+      </ul>
      <Board />
     </div>
   )
